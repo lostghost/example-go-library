765bb0139a89b2821c04f93a50b3356a761d1ccd
Ken Walters <ken@lostghost.com>
2017-04-07 12:52:59 -0400

    v0.0.1

---
+++ lib.go
@@ -0,0 +1,9 @@
+package examplegolibrary
+
+// Version is the current application version identifier
+const Version = "v0.0.1"
+
+// GetVersion returns the applicaiton version identifier
+func GetVersion() string {
+	return Version
+}
---
+++ lib_test.go
@@ -0,0 +1,19 @@
+package examplegolibrary
+
+import (
+	"testing"
+)
+
+var expectedVersion = "v0.0.1"
+
+func TestVersion(t *testing.T) {
+	if expectedVersion != Version {
+		t.Errorf("Unexpected version number from Version: expected %s, got %s", expectedVersion, Version)
+	}
+}
+
+func TestGetVersion(t *testing.T) {
+	if expectedVersion != GetVersion() {
+		t.Errorf("Unexpected version number from GetVersion: expected %s, got %s", expectedVersion, GetVersion())
+	}
+}
